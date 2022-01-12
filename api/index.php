5c272ddab2c27a815577a0c2be631af94d15012e
Shaswat Raj <66713844+SH20RAJ@users.noreply.github.com>
2022-01-12 16:44:59 +0530

    one shot coded

--- api/index.php
+++ api/index.php
@@ -1,1 +1,12 @@
+<?php
+if(isset($_GET['video'])) {
+   $json = file_get_contents($_GET['video']."?__a=1");
+    //Getting the file content
+    $json = json_decode($json);
+    //Converting the JSON into Php object
 
+    //Let's get the images into $html and image array to $arr... 
+    $arr = $json->graphql->shortcode_media->video_url;
+    header("Location: ".$arr."");
+}
+?>
